2fcff2e08b7b666ba1167eb8a0db2cab0d96c25c
InbalBenItamar <inbal.benitamar@gmail.com>
2018-06-12 23:22:05 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 48px;
+  text-align: center;
+  color: #581845;
+`;
+
+const Text = styled.p`
+  font-size: 22px;
+  text-align: center;
+  color: #C70039;
+`;
+
+const App = () => {
+  return (
+    <div>
+      <Title>Hello</Title>
+      <Text>I'm a styled component!</Text>
+    </div>
+  );
+};
+export default App;
+ReactDOM.render(<App />, document.getElementById("app"));
